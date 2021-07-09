7f404f9663e7a2db49a21a12ec7629e6498d2e10
Shanto <shaniulshanto.2331.csedu@gmail.com>
2021-07-10 05:40:09 +0600

    from shanto

---
+++ app/src/main/java/com/example/stopthecrime/HelplineActivity.java
@@ -0,0 +1,157 @@
+package com.example.stopthecrime;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HelplineActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CALL = 1;
+    private ArrayList<String> data = new ArrayList<String>();
+
+    String policePhoneNumberString = "999";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helpline);
+        ListView lv = findViewById(R.id.thanaListView);
+
+        generateListContent();
+
+        lv.setAdapter(new MyListAdaper(HelplineActivity.this, R.layout.thana_list_item, data));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(HelplineActivity.this, "List item was clicked at " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button BackButton = findViewById(R.id.backButton);
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(20);
+                finish();
+            }
+        });
+
+    }
+
+    private void generateListContent() {
+        for (int i = 1; i <= 20; i++) {
+            data.add("Thana " + i + "\n" + "Information");
+        }
+    }
+
+    private class MyListAdaper extends ArrayAdapter<String> {
+        private int layout;
+        private List<String> mObjects;
+
+        private MyListAdaper(HelplineActivity context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                //viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.list_item_thumbnail);
+                viewHolder.title = (TextView) convertView.findViewById(R.id.thanaItemText);
+                viewHolder.button = (Button) convertView.findViewById(R.id.thanaCallButton);
+
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View v) {
+                    Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vibe.vibrate(20);
+                    //Ask For Permission Required HERE
+                    callPoliceWithPermission();
+                }
+            });
+            mainViewholder.title.setText(getItem(position));
+
+            return convertView;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void callPoliceWithPermission() {
+        if (ActivityCompat.checkSelfPermission(HelplineActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(HelplineActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            callPolice();
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callPolice();
+            } else {
+                Toast.makeText(this, "Please Grant Permission to Make The Call!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void callPolice() {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + policePhoneNumberString));
+        if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
+    }
+
+    public class ViewHolder {
+        TextView title;
+        Button button;
+    }
+
+}
+
---
+++ app/src/main/java/com/example/stopthecrime/ProfileActivity.java
@@ -0,0 +1,150 @@
+package com.example.stopthecrime;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private String UID;
+    private TextView userNameText;
+    private TextView userEmailText;
+    private TextView userGenderText;
+
+    //Firebase Database Node Heads are named as FirebaseAuth:UserIDs
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+    //And Then DatabaseReference references the FirebaseDatabase with the UID to retrieve profile information
+    DatabaseReference databaseReference = firebaseDatabase.getReference("user");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        userNameText = (TextView) findViewById(R.id.userName);
+        userEmailText = (TextView) findViewById(R.id.userEmail);
+        userGenderText = (TextView) findViewById(R.id.userGender);
+
+        //FirebaseUser Retrieves UID from FirebaseAuth
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser!=null){
+            UID = firebaseUser.getUid();
+        }
+
+        //DataSnapshot on node.user -> node.uid -> ...
+        databaseReference.child(UID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NotNull @NonNull DataSnapshot dataSnapshot) {
+                try{
+//                    UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+//                    String nameLabel = "Name:  ";
+//                    userNameText.setText(nameLabel.concat(userProfile.getName()));
+//                    String emailLabel = "Email:  ";
+//                    userEmailText.setText(emailLabel.concat(userProfile.getEmail()));
+//                    String genderLabel = "Gender:  ";
+//                    userGenderText.setText(genderLabel.concat(userProfile.getGender()));
+                    //Toast.makeText(ProfileActivity.this, "Profile Data Loaded Successfully",Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(ProfileActivity.this, "Unable to Load Profile Data. "/*e.toString()*/, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ProfileActivity.this, "Unable To Reach Database. ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button editProfileData = findViewById(R.id.editProfileDataButton);
+        editProfileData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(20);
+                Toast.makeText(ProfileActivity.this, "Not Implemented Yet ! ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button stopBackgroundService = findViewById(R.id.stopBackgroundServiceButton);
+        stopBackgroundService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(100);
+                databaseReference.child(UID).child("shareLoc").setValue(false);
+                databaseReference.child(UID).child("shareLoc").setValue(false);
+                databaseReference.child(UID).child("shareLoc").setValue(false);
+                Toast.makeText(ProfileActivity.this, "Location Share Stopped!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button BackButton = findViewById(R.id.backButton);
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(20);
+                finish();
+            }
+        });
+
+        Button logoutButton = findViewById(R.id.logOutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(50);
+                Toast.makeText(ProfileActivity.this, "Press and Hold Button To Logout", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        logoutButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(20);
+                SignOut();
+                finish();
+                return false;
+            }
+        });
+
+    }
+
+    private void SignOut(){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+    }
+
+    private void killFirstActivity(){
+        try{
+//
+//           // Intent intent = new Intent(ProfileActivity.this, NavDrawerActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            intent.putExtra("EXIT", true);
+//            startActivity(intent);
+        }catch (Exception e){
+            Toast.makeText(ProfileActivity.this, "Background service not running.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
+
